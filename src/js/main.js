a1483d80f05f7a102217c3451285a0ccea4949d3
Rainum <rainum.ua@gmail.com>
2013-03-14 17:59:23 +0200

    profile settings, form validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,12 @@
-(function ($) {
+(function ($, win) {
     var $loginModal = $('#login-modal'),
         $authSteps = $loginModal.find('.auth-step'),
         $authForm = $('#auth-form').find('form'),
         $forgotForm = $('#auth-forgot-pswd').find('form'),
         $regForm = $('#register-modal').find('form');
 
-    function updateValidationMessage(field) {
-        $(field).next('.error').html(field.validationMessage);
-    }
-
-    function validateForm($form) {
-        var $required = $form.find(':required'),
-            valid = $form.get(0).checkValidity();
-
-        if (!valid) {
-            $form.addClass('submitted');
-
-            $required.each(function(i, field) {
-                updateValidationMessage(field);
-            });
-        } else {
-            // Post form data on validation success
+    function submitForm($form) {
+        if ($form.parsley('isValid')) {
             $.ajax({
                 url: $form.attr('action'),
                 type: 'post',
@@ -28,39 +14,75 @@
             }).done(function() {
                     console.log('Here must be redirect');
                 });
+        } else {
+            toggleButtonState($form, false);
         }
-
-        //updateButtonState($form, !valid);
     }
 
-    function updateButtonState($form, state) {
-        var data = $form.serializeArray(),
-            $btn = $form.find('input[type="submit"]'),
-            errText,
-            disableBtn = false;
+    function toggleButtonState($form, enableBtn) {
+        var $btn = $form.find('input[type="submit"]'),
+            errText;
 
-        /*if (typeof state !== 'undefined') {
-            $btn.attr('disabled', state);
-
-            return;
-        }*/
-
-        for (var i = 0; i < data.length; i++) {
-            if (!data[i].value) {
-                disableBtn = true;
-                errText = data[i].name === 'email' ? 'e-mail' : 'пароль';
-            }
-        }
-
-        $btn.attr('disabled', disableBtn)
-            .next('.error').html('Введите ' + errText).toggle(disableBtn);
+        $btn.attr('disabled', !enableBtn);
+            //.next('.error').html('Введите ' + errText).toggle(disableBtn);
     }
+
+    // Auth form validation
+    $authForm.on('submit',function (e) {
+        e.preventDefault();
+        submitForm($(this));
+    }).find('[data-required]').on('keyup', function () {
+            var enableBtn = true;
+
+            $authForm.find('[data-required]').each(function(i, field) {
+                var $field = $(field),
+                    $pField = $field.parsley();
+
+                if (!$field.val() || $pField.validatedOnce) {
+                    enableBtn = false;
+                    return false;
+                }
+            });
+
+            toggleButtonState($authForm, enableBtn);
+        });
+
+    // Parsley - form validation plugin
+
+    win.ParsleyConfig = $.extend( true, {}, win.ParsleyConfig, {
+        messages: {
+            defaultMessage: "Поле заполнено некорректно.",
+            type: {
+                email: "Поле должно быть адресом электронной почты.",
+                url: "Поле должно быть ссылкой на сайт.",
+                urlstrict: "Поле должно быть ссылкой на сайт.",
+                number: "Поле должно быть числом.",
+                digits: "Поле должно содержать только цифры.",
+                dateIso: "Поле должно быть датой в формате (ГГГГ-ММ-ДД).",
+                alphanum: "Поле должно содержать только  цифры и буквы"
+            },
+            notnull: "Поле должно быть не нулевым.",
+            notblank: "Поле не должно быть пустым.",
+            required: "Поле обязательно для заполнения.",
+            regexp: "Поле заполнено некорректно.",
+            min: "Значение поля должно быть больше %s.",
+            max: "Значение поля должно быть меньше %s.",
+            range: "Значение поля должно быть между %s и %s.",
+            minlength: "В поле должно быть минимум %s символов(а).",
+            maxlength: "В поле должно быть не больше %s символов(а).",
+            rangelength: "В поле должно быть от %s до %s символов(а).",
+            mincheck: "Необходимо выбрать не менее %s пунктов(а).",
+            maxcheck: "Необходимо выбрать не более %s пунктов(а).",
+            rangecheck: "Необходимо выбрать от %s до %s пунктов.",
+            equalto: "Значения полей должны быть одинаковыми."
+        }
+    });
 
     // Reset modal steps state
     $loginModal.on('show', function () {
         $authSteps.hide().eq(0).show();
         $authForm.removeClass('submitted').get(0).reset();
-        $authForm.find('input[type="submit"]').attr('disabled', true);
+        //$authForm.find('input[type="submit"]').attr('disabled', true);
     });
 
     // Handle modal auth steps
@@ -74,14 +96,14 @@
     });
 
     // Auth form validation
-    $authForm.on('submit', function(e) {
+    /*$authForm.on('submit', function(e) {
         e.preventDefault();
         validateForm($(this));
     }).find(':required').on('change', function() {
             updateValidationMessage(this);
         }).on('keyup', function() {
                 updateButtonState($authForm);
-            });
+            });*/
 
     // Forgot password form validation
     $forgotForm.on('submit', function(e) {
@@ -102,4 +124,4 @@
         }).on('keyup', function() {
             updateButtonState($regForm);
         });
-}(jQuery));
+}(jQuery, window));
